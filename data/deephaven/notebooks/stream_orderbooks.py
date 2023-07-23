511d4fe40016b6a4b5807fff2c538608785ffe04
kzk2000 <kilianm@gmail.com>
2023-07-22 23:44:11 -0400

    latest state, orderbook stuff not all ironed out yet

--- data/deephaven/notebooks/stream_orderbooks.py
+++ data/deephaven/notebooks/stream_orderbooks.py
@@ -8,13 +8,15 @@ import deephaven.pandas as dhpd
 from deephaven.plot.figure import Figure
 from deephaven.plot import LineEndStyle, LineJoinStyle, LineStyle, Colors
 import deephaven.stream.kafka.consumer as ck
-from deephaven import agg, merge
+from deephaven import agg, merge, ring_table
 from deephaven.table import Table
+from deephaven.time import second_of_day, time_zone, minute_of_day
 import deephaven
+
 import deephaven_tools as tools
 
 
-# stream table from Kafka
+# blink table from Kafka
 orderbooks = ck.consume(
   {'bootstrap.servers': 'redpanda:29092'},
   'orderbooks',
@@ -28,9 +30,12 @@ orderbooks = ck.consume(
   ]),
   table_type=ck.TableType.blink())\
   .update_view([
-    #'is_db = (long) 0',
-    'ts_bin = lowerBin(ts, SECOND)',
+    'ts_bin = lowerBin(ts, 1 * SECOND)',
+    'ts_cycle = (int) second_of_day(ts, time_zone(`ET`))',
   ])
+
+# downsample to 1 second intervals, 'ts_cycle' ensures memory growth to be finite (using ts_bin would blow up memory eventually)
+order_books_sampled = orderbooks.last_by(['symbol', 'ts_cycle']).sort(['ts_bin'])
 
 
 def extract_book_level(x: str, level: int, field: str) -> np.float64:
@@ -41,7 +46,7 @@ def extract_book_level(x: str, level: int, field: str) -> np.float64:
         return list(json.loads(x).values())[level]
 
 
-quotes_l1_stream = orderbooks.view([
+quotes_l1 = order_books_sampled.select([
   'ts',
   'ts_bin',
   'symbol',
@@ -54,44 +59,46 @@ quotes_l1_stream = orderbooks.view([
   'spread_bps = spread / mid * 10000',
 ])
 
-# create ring table
-quotes_l1_ring = tools.make_ring(quotes_l1_stream, 200000).tail_by(50000, ['symbol', 'ts_bin'])
+# create ring table -> this throws...need to file bug report
+# quotes_l1_ring = ring_table(quotes_l1, 40)
 
 quotes_per_second = orderbooks\
-  .agg_by([agg.count_('count')], by=["ts_bin"])\
+  .agg_by([agg.last('ts_bin'), agg.count_('count')], by=["ts_cycle"])\
   .tail(10)
 
-quotes_one_symbol = quotes_l1_ring.where(['symbol == `BTC-USD`'])
-
+quotes_one_symbol = quotes_l1.where(['symbol == `BTC-USD`'])
 
 plot_bid_vs_ask = Figure()\
   .plot_xy(series_name="BID", t=quotes_one_symbol, x="ts", y="bid")\
   .plot_xy(series_name="ASK", t=quotes_one_symbol, x="ts", y="ask")\
   .show()
 
-#plot bid-ask spread
+# plot bid-ask spread
 plot_spread_bps = Figure()\
   .plot_xy(series_name="SPREAD_BPS", t=quotes_one_symbol, x="ts", y="spread_bps")\
   .show()
 
 
-trades_n_quotes = trades.aj(table=quotes_l1_ring , on=["symbol", "ts"])
-trades_n_quotesA = trades_n_quotes.where(['symbol == `BTC-USD`']).tail(300)
+# Note: quote_l1 is subsampled here
+trades_and_quotes = trades.aj(table=quotes_l1, on=["symbol", "ts"])
+trades_and_quotes_one_symbol = trades_and_quotes.where(['symbol == `BTC-USD`']).tail(300)
 
 plot_bid_vs_ask = Figure()\
   .chart_title(title="Trades & Quotes")\
-  .plot_xy(series_name="TRD_BUY", t=trades_n_quotesA.where('side==`buy`'), x="ts", y="price")\
-  .plot_xy(series_name="TRD_SELL", t=trades_n_quotesA.where('side==`sell`'), x="ts", y="price")\
+  .plot_xy(series_name="TRD_BUY", t=trades_and_quotes_one_symbol.where('side==`buy`'), x="ts", y="price")\
+  .plot_xy(series_name="TRD_SELL", t=trades_and_quotes_one_symbol.where('side==`sell`'), x="ts", y="price")\
   .axes(plot_style=PlotStyle.SCATTER)\
   .twin()\
-  .plot_xy(series_name="BID", t=trades_n_quotesA, x="ts", y="bid")\
-  .plot_xy(series_name="ASK", t=trades_n_quotesA, x="ts", y="ask")\
+  .plot_xy(series_name="BID", t=trades_and_quotes_one_symbol, x="ts", y="bid")\
+  .plot_xy(series_name="ASK", t=trades_and_quotes_one_symbol, x="ts", y="ask")\
   .axes(plot_style=PlotStyle.STEP)\
   .show()
  
 
-# 
-f = Figure(rows=1, cols=2)\
+plot_2subplots = Figure(rows=1, cols=2)\
     .new_chart(row=0, col=0).plot_xy(series_name="SPREAD", t=quotes_one_symbol, x="ts", y="spread")\
     .new_chart(row=0, col=1).plot_xy(series_name="SPREAD_BPS", t=quotes_one_symbol, x="ts", y="spread_bps")\
     .show()
+
+
+
--- docker/clickhouse/sql/init_orderbooks.sql
+++ docker/clickhouse/sql/init_orderbooks.sql
@@ -3,11 +3,13 @@
 --DROP TABLE IF EXISTS cryptofeed.orderbooks;
 --DROP TABLE IF EXISTS cryptofeed.orderbooks_queue;
 --DROP VIEW IF EXISTS cryptofeed.orderbooks_queue_mv;
+--DROP TABLE IF EXISTS cryptofeed.orderbooks_out_queue;
+--DROP VIEW IF EXISTS cryptofeed.orderbooks_out_queue_mv;
 
 -- create database schema
 CREATE DATABASE IF NOT EXISTS cryptofeed;
 
--- create table for orderbooka
+-- create table for ALL orderbook snapshots
 CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks
 (
     exchange        LowCardinality(String)
@@ -49,4 +51,76 @@ SELECT
   , cast(JSONExtractKeysAndValues(raw, 'ask', 'Float64'), 'Map(String, Float64)')                 AS ask
   , _offset                                                                                       AS KafkaOffset
 FROM cryptofeed.orderbooks_queue;
+
+
+-- everything above this line pushes ALL orderbook snapshots into cryptofeed.orderbooks
+-- To downsample them to only 1 snapshot per second and push that back out to Kafka
+-- to do this, we follow the steps in https://clickhouse.com/docs/en/integrations/kafka#2-utilizing-materialized-views
+
+-- outbound Kafka topic 'orderbooks_1sec', this is the one we subscribe to on DH side
+CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks_out_queue
+(
+    exchange        LowCardinality(String)
+    , symbol        LowCardinality(String)
+    , ts_latest     UInt64
+    , ts_1sec       UInt64
+    , bid           Map(String, Float64)
+    , ask           Map(String, Float64)
+)
+ENGINE = Kafka
+SETTINGS
+    kafka_broker_list = 'redpanda:29092',
+    kafka_topic_list = 'orderbooks_1sec',
+    kafka_group_name = 'clickhouse',
+    kafka_format = 'JSONEachRow',
+    kafka_flush_interval_ms = 1000,
+    kafka_thread_per_consumer = 0,
+    kafka_num_consumers = 1;
+
+-- aggregates orderbook snapshots to 1sec resolution and pushes them onto the outgoing Kafka queue
+CREATE MATERIALIZED VIEW IF NOT EXISTS cryptofeed.orderbooks_out_queue_mv TO cryptofeed.orderbooks_out_queue AS
+SELECT
+  exchange
+  , symbol
+  , toUnixTimestamp64Nano(max(ts))                  AS ts_latest
+  , toUnixTimestamp64Nano(max(toStartOfSecond(ts))) AS ts_1sec
+  , argMax(bid, ts)                                 AS bid
+  , argMax(ask, ts)                                 AS ask
+FROM cryptofeed.orderbooks
+WHERE
+  ts >= ts - INTERVAL 15 SECOND
+GROUP BY exchange, symbol;
+
+
+-- older version: keep for reference for now
+--
+--CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks_out_queue
+--(
+--    exchange        LowCardinality(String)
+--    , symbol        LowCardinality(String)
+--    , ts_1sec       UInt32
+--    , bid           Map(String, Float64)
+--    , ask           Map(String, Float64)
+--)
+--ENGINE = Kafka
+--SETTINGS
+--    kafka_broker_list = 'redpanda:29092',
+--    kafka_topic_list = 'orderbooks_1sec',
+--    kafka_group_name = 'clickhouse',
+--    kafka_format = 'JSONEachRow',
+--    kafka_flush_interval_ms = 1000,
+--    kafka_thread_per_consumer = 0,
+--    kafka_num_consumers = 1;
+--
+---- aggregates orderbook snapshots to 1sec resolution and pushes them onto the outgoing Kafka queue
+--CREATE MATERIALIZED VIEW IF NOT EXISTS cryptofeed.orderbooks_out_queue_mv TO cryptofeed.orderbooks_out_queue AS
+--SELECT
+--    exchange
+--    , symbol
+--    , toUnixTimestamp(toStartOfSecond(ts)) AS ts
+--    , toUnixTimestamp(toStartOfSecond(ts)) AS ts_1sec
+--    , argMax(bid, ts)                      AS bid
+--    , argMax(ask, ts)                      AS ask
+--FROM cryptofeed.orderbooks
+--GROUP BY ts_1sec, symbol, exchange;
 
