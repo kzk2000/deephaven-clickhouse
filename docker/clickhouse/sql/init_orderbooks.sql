21a8cd013c8d6c69f8861ad198c80b80b1ad992d
kzk2000 <kilianm@gmail.com>
2024-03-31 15:51:29 -0400

    add 1sec AggregatedMergeTree for orderbook snapshots

--- docker/clickhouse/sql/init_orderbooks.sql
+++ docker/clickhouse/sql/init_orderbooks.sql
@@ -2,15 +2,56 @@
 -- uncomment these to start from scratch
 --DROP TABLE IF EXISTS cryptofeed.orderbooks;
 --DROP TABLE IF EXISTS cryptofeed.orderbooks_queue;
---DROP VIEW IF EXISTS cryptofeed.orderbooks_queue_mv;
+--DROP VIEW IF EXISTS cryptofeed.orderbooks_1sec_mv;
+--DROP VIEW IF EXISTS cryptofeed.orderbooks_all_mv;
+
 --DROP TABLE IF EXISTS cryptofeed.orderbooks_out_queue;
 --DROP VIEW IF EXISTS cryptofeed.orderbooks_out_queue_mv;
 
 -- create database schema
 CREATE DATABASE IF NOT EXISTS cryptofeed;
 
--- create table for ALL orderbook snapshots
---CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks
+-----------------------------------------------------------------------------------------------------------
+-- create Kafka table engine, flush every 1000ms
+CREATE OR REPLACE TABLE cryptofeed.orderbooks_queue
+(
+  exchange      LowCardinality(String)
+  , symbol      LowCardinality(String)
+  , ts          DateTime64(9)
+  , bid         Map(String, Float64)
+  , ask         Map(String, Float64)
+) ENGINE = Kafka
+SETTINGS
+    kafka_broker_list = 'redpanda:29092',
+    kafka_topic_list = 'orderbooks',
+    kafka_group_name = 'clickhouse',
+    kafka_format = 'JSONEachRow',
+    kafka_flush_interval_ms = 1000,
+    kafka_skip_broken_messages = 1;
+
+-----------------------------------------------------------------------------------------------------------
+-- create AggregatingMergeTree, keeps the LAST orderbook snapshot per second (once merging finalizes)
+CREATE MATERIALIZED VIEW IF NOT EXISTS cryptofeed.orderbooks_1sec_mv
+(
+    exchange        LowCardinality(String)
+    , symbol        LowCardinality(String)
+    , ts_bin        DateTime64(9)
+    , bid           SimpleAggregateFunction(anyLast, Map(String, Float64))
+    , ask           SimpleAggregateFunction(anyLast, Map(String, Float64))
+)
+ENGINE = AggregatingMergeTree
+ORDER BY (exchange, symbol, ts_bin)
+AS SELECT
+  exchange
+  , symbol
+  , toStartOfSecond(ts) AS ts_bin
+  , argMax(bid, ts)     AS bid
+  , argMax(ask, ts)     AS ask
+FROM cryptofeed.orderbooks_queue
+GROUP BY (exchange, symbol, ts_bin);
+
+-----------------------------------------------------------------------------------------------------------
+-- create MergeTree, stores ALL orderbook snapshots (only keeps 1 HOUR of history)
 CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks
 (
     exchange        LowCardinality(String)
@@ -18,86 +59,28 @@ CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks
     , ts            DateTime64(9)
     , bid           Map(String, Float64)
     , ask           Map(String, Float64)
-    , KafkaOffset   Int64
 ) Engine = MergeTree
 PARTITION BY toYYYYMM(ts)
-TTL toDateTime(ts - INTERVAL 1 DAY)
+TTL toDateTime(ts + INTERVAL 1 HOUR)
 ORDER BY (symbol, exchange, ts);
 
--- create Kafka table engine, flush every 1000ms
-CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks_queue
-(
-  raw String
-) ENGINE = Kafka
-SETTINGS
-    kafka_broker_list = 'redpanda:29092',
-    kafka_topic_list = 'orderbooks',
-    kafka_group_name = 'clickhouse',
-    kafka_format = 'JSONAsString',
-    kafka_flush_interval_ms = 1000,
-    kafka_thread_per_consumer = 0,
-    kafka_num_consumers = 1,
-    kafka_skip_broken_messages = 1;
-
--- materialized view that persists the Kafka table engine to cryptofeed.orderbooks
--- _offset is a virtual column, see https://clickhouse.com/docs/en/engines/table-engines/integrations/kafka#virtual-columns
-CREATE MATERIALIZED VIEW IF NOT EXISTS cryptofeed.orderbooks_queue_mv TO cryptofeed.orderbooks AS
-SELECT
-  JSONExtractString(raw, 'exchange')                                                              AS exchange
-  , JSONExtractString(raw, 'symbol')                                                              AS symbol
-  , toDateTime64(JSONExtractString(raw, 'ts'), 9)                                                 AS ts
-  , cast(JSONExtractKeysAndValues(raw, 'bid', 'Float64'), 'Map(String, Float64)')                 AS bid
-  , cast(JSONExtractKeysAndValues(raw, 'ask', 'Float64'), 'Map(String, Float64)')                 AS ask
-  , _offset                                                                                       AS KafkaOffset
-FROM cryptofeed.orderbooks_queue;
+-- create materialized view, pushing ALL orderbooks snapshots from Kafka to cryptofeed.orderbooks
+CREATE MATERIALIZED VIEW IF NOT EXISTS cryptofeed.orderbooks_all_mv TO cryptofeed.orderbooks AS
+SELECT * FROM cryptofeed.orderbooks_queue;
 
 
--- everything above this line pushes ALL orderbook snapshots into cryptofeed.orderbooks
--- To downsample them to only 1 snapshot per second and push that back out to Kafka
--- to do this, we follow the steps in https://clickhouse.com/docs/en/integrations/kafka#2-utilizing-materialized-views
+---------------------------------------------------------------------------------------------------------------------------------------
+-- everything above this line pushes ALL orderbook snapshots into 'cryptofeed.orderbooks' and 'cryptofeed.orderbooks_1sec_mv'
+-- below this line creates an outbound Kafka topic that pushes from Clickhouse back out to Kafka (experimental)
+-- to do this, we follow the steps in https://clickhouse.com/docs/en/integrations/kafka/kafka-table-engine#2-using-materialized-views
 
--- outbound Kafka topic 'orderbooks_1sec', this is the one we subscribe to on DH side
-CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks_out_queue
-(
-    exchange        LowCardinality(String)
-    , symbol        LowCardinality(String)
-    , ts_latest     UInt64
-    , ts_1sec       UInt64
-    , bid           Map(String, Float64)
-    , ask           Map(String, Float64)
-)
-ENGINE = Kafka
-SETTINGS
-    kafka_broker_list = 'redpanda:29092',
-    kafka_topic_list = 'orderbooks_1sec',
-    kafka_group_name = 'clickhouse',
-    kafka_format = 'JSONEachRow',
-    kafka_flush_interval_ms = 1000,
-    kafka_thread_per_consumer = 0,
-    kafka_num_consumers = 1;
-
--- aggregates orderbook snapshots to 1sec resolution and pushes them onto the outgoing Kafka queue
-CREATE MATERIALIZED VIEW IF NOT EXISTS cryptofeed.orderbooks_out_queue_mv TO cryptofeed.orderbooks_out_queue AS
-SELECT
-  exchange
-  , symbol
-  , toUnixTimestamp64Nano(max(ts))                  AS ts_latest
-  , toUnixTimestamp64Nano(max(toStartOfSecond(ts))) AS ts_1sec
-  , argMax(bid, ts)                                 AS bid
-  , argMax(ask, ts)                                 AS ask
-FROM cryptofeed.orderbooks
-WHERE
-  ts >= ts - INTERVAL 15 SECOND
-GROUP BY exchange, symbol;
-
-
--- older version: keep for reference for now
---
+-- outbound Kafka topic 'orderbooks_1sec'
 --CREATE TABLE IF NOT EXISTS cryptofeed.orderbooks_out_queue
 --(
 --    exchange        LowCardinality(String)
 --    , symbol        LowCardinality(String)
---    , ts_1sec       UInt32
+--    , ts_latest     UInt64
+--    , ts_1sec       UInt64
 --    , bid           Map(String, Float64)
 --    , ask           Map(String, Float64)
 --)
@@ -111,15 +94,16 @@ GROUP BY exchange, symbol;
 --    kafka_thread_per_consumer = 0,
 --    kafka_num_consumers = 1;
 --
----- aggregates orderbook snapshots to 1sec resolution and pushes them onto the outgoing Kafka queue
+-- aggregates orderbook snapshots to 1sec resolution and pushes them onto the outgoing Kafka queue
 --CREATE MATERIALIZED VIEW IF NOT EXISTS cryptofeed.orderbooks_out_queue_mv TO cryptofeed.orderbooks_out_queue AS
 --SELECT
---    exchange
---    , symbol
---    , toUnixTimestamp(toStartOfSecond(ts)) AS ts
---    , toUnixTimestamp(toStartOfSecond(ts)) AS ts_1sec
---    , argMax(bid, ts)                      AS bid
---    , argMax(ask, ts)                      AS ask
+--  exchange
+--  , symbol
+--  , toUnixTimestamp64Nano(max(ts))                  AS ts_latest
+--  , toUnixTimestamp64Nano(max(toStartOfSecond(ts))) AS ts_1sec
+--  , argMax(bid, ts)                                 AS bid
+--  , argMax(ask, ts)                                 AS ask
 --FROM cryptofeed.orderbooks
---GROUP BY ts_1sec, symbol, exchange;
-
+--WHERE
+--  ts >= ts - INTERVAL 15 SECOND
+--GROUP BY exchange, symbol;
